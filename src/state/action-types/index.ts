bb2226afa8df7bb2ddbcdbd25e6a656c1fc2cbe7
Kevin O'Malley <kevin.omalley@cleverbridge.com>
2021-01-27 09:10:32 -0600

    starter file structure

---
+++ src/state/action-types/index.ts
@@ -0,0 +1,6 @@
+// enum is like an object
+export enum ActionType {
+    SEARCH_REPOSITORIES = 'search_repositories',
+    SEARCH_REPOSITORIES_SUCCESS = 'search_repositories_success',
+    SEARCH_REPOSITORIES_ERROR = 'search_repositories_error',
+}
